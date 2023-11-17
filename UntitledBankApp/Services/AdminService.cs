7bc76575b231100fcd3f66f40826497a11baa39b
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-11-17 15:39:53 +0100

    Checks if username is already in use.

--- UntitledBankApp/Services/AdminService.cs
+++ UntitledBankApp/Services/AdminService.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 using UntitledBankApp.Models;
 
 namespace UntitledBankApp.Services;
@@ -14,19 +16,41 @@ public class AdminService
     public bool CreateUser(Role role, string fullname, string username, string password)
     {
         /*
-         * TODO Service: Create User objects.
-         *
-         * Use the parameters to:
-         * 
-         * Search the pseudoDb Users List. Find any duplicate usernames.
-         * Usernames must be unique, since they're used during login.
-         * If there is another User object with the same name, return false to the method caller.
-         * Otherwise, create a User object: Client or Admin depending on the value of "role".
-         * Add this object to the Users List in pseudoDb and return true.
-         *
-         */
+* TODO Service: Create User objects.
+*
+* Use the parameters to:
+* 
+* Search the pseudoDb Users List. Find any duplicate usernames.
 
-        return true;
+* Usernames must be unique, since they're used during login.
+* If there is another User object with the same name, return false to the method caller.
+* Otherwise, create a User object: Client or Admin depending on the value of "role".
+* Add this object to the Users List in pseudoDb and return true.
+*
+*/
+
+
+     
+        
+        bool checkUsername = _pseudoDb.Users.Exists(u => u.Username == username);
+        if (checkUsername)
+        {
+            return false;
+        }
+        else if(Role.Admin is Role)
+        {
+            Admin newAdmin = new Admin("NewFullName", username,"NewPassword");
+            _pseudoDb.Users.Add(newAdmin);
+            return true;
+        }
+        else
+        {
+            Client newClient = new Client("NewFullName", username, "NewPassword");
+            _pseudoDb.Users.Add(newClient);
+            return true;
+        }
+        
+
     }
 
     public void SetCurrencyRate(CurrencyCode currencyCode, float rate)
