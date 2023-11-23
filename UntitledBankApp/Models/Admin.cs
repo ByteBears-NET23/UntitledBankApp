e6463d410be308e6b1d9ad2eebe9b8876ade30a0
Yarub1 <144337494+Yarub1@users.noreply.github.com>
2023-11-23 14:52:46 +0100

    Update Admin.cs

--- UntitledBankApp/Models/Admin.cs
+++ UntitledBankApp/Models/Admin.cs
@@ -5,4 +5,5 @@ public class Admin : User
     public Admin(string fullName, string username, string password) : base(Role.Admin, fullName, username, password)
     {
     }
+    public AdminService AdminService { get; internal set; }
 }
