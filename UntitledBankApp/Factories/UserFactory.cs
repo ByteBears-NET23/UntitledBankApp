eed2eb7aa2a01183e121b9f8e5ed2c1b7e1a2133
AdrianMorenoNystrom <adrianmorenonystrom@gmail.com>
2023-11-23 21:22:24 +0100

    New UserFactory branch to test if this works without conflicts. I hope i don't missed anything from the pull request from the first branch.

---
+++ UntitledBankApp/Factories/UserFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UntitledBankApp.Factories
+{
+    public class UserFactory
+    {
+        public User CreateUser(Role role, string fullname, string username, string password)
+        {
+            switch (role)
+            {
+                case Role.Admin:
+                    return new Admin(fullname, username, password);
+                case Role.Client:
+                    return new Client(fullname, username, password);
+                default:
+                    throw new ArgumentException("Invalid role");
+            }
+        }
+    }
+}
+
