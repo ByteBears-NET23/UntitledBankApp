09922b26dce89d6989480b7c1e734ddba2904991
Yarub1 <144337494+Yarub1@users.noreply.github.com>
2023-11-24 10:53:21 +0100

    Update ClientView.cs

--- UntitledBankApp/Views/ClientView.cs
+++ UntitledBankApp/Views/ClientView.cs
@@ -19,166 +19,40 @@ namespace UntitledBankApp.Views
 
         protected override void DisplayHeader()
         {
-            Console.WriteLine($"Welcome, {_client.FullName}!");
-            Console.WriteLine("*************************");
+            //Console.WriteLine($"Welcome, {_client.Name}!");
         }
 
         public void DisplayClientMenu()
         {
-            bool exitMenu = false;
-
-            while (!exitMenu)
-            {
-                DisplayHeader();
-                Console.WriteLine("1. Create Account");
-                Console.WriteLine("2. Request Loan");
-                Console.WriteLine("3. View Accounts");
-                Console.WriteLine("4. Transfer Funds");
-                Console.WriteLine("5. Exit");
-
-                Console.Write("Select an option (1-5): ");
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        CreateAccount();
-                        break;
-                    case "2":
-                        RequestLoan();
-                        break;
-                    case "3":
-                        ViewAccounts();
-                        break;
-                    case "4":
-                        TransferFunds();
-                        break;
-                    case "5":
-                        exitMenu = true;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid option. Please try again.");
-                        break;
-                }
-            }
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. View Accounts");
+            Console.WriteLine("3. Request Loan");
+            Console.WriteLine("4. Transfer Money");
+            Console.WriteLine("5. Exit");
         }
 
-        private void CreateAccount()
+        public string GetClientMenuChoice()
         {
-            Console.WriteLine("****** Create Account ******");
-
-            // Assuming you have a way to select the currency code.
-            Console.Write("Enter Currency Code (e.g., USD): ");
-            string currencyCodeStr = Console.ReadLine();
-
-            if (Enum.TryParse(currencyCodeStr, out CurrencyCode currencyCode))
-            {
-                bool result = _clientService.CreateAccount(_client, currencyCode);
-                Console.WriteLine(result ? "Account created successfully." : "Failed to create account.");
-            }
-            else
-            {
-                Console.WriteLine("Invalid currency code.");
-            }
-
-            Console.WriteLine("Press Enter to continue...");
-            Console.ReadLine();
+            Console.Write("Enter your choice: ");
+            return Console.ReadLine();
         }
 
-        private void RequestLoan()
+        public string GetAccountInfo()
         {
-            Console.WriteLine("****** Request Loan ******");
-
-            // Assuming you have a way to select an account for the loan.
-            Console.Write("Enter Account Number: ");
-            if (int.TryParse(Console.ReadLine(), out int accountNumber))
-            {
-                var account = _client.Accounts.FirstOrDefault(a => a.Number == accountNumber);
-
-                if (account != null)
-                {
-                    bool result = _clientService.Loan(account);
-                    Console.WriteLine(result ? "Loan request successful." : "Loan request failed.");
-                }
-                else
-                {
-                    Console.WriteLine("Account not found.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid account number.");
-            }
-
-            Console.WriteLine("Press Enter to continue...");
-            Console.ReadLine();
+            Console.Write("Enter account information: ");
+            return Console.ReadLine();
         }
 
-        private void ViewAccounts()
+        public string GetLoanAmount()
         {
-            Console.WriteLine("****** View Accounts ******");
-
-            // Assuming you have a way to display the client's accounts.
-            var accounts = _clientService.GetAccounts(_client);
-            if (accounts != null)
-            {
-                foreach (var account in accounts)
-                {
-                    Console.WriteLine($"Account Number: {account.Number}, Balance: {account.Balance.Amount} {account.Balance.Currency.Code}");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No accounts found for the client.");
-            }
-
-            Console.WriteLine("Press Enter to continue...");
-            Console.ReadLine();
+            Console.Write("Enter loan amount: ");
+            return Console.ReadLine();
         }
 
-        private void TransferFunds()
+        public void ShowMessage(string message)
         {
-            Console.WriteLine("****** Transfer Funds ******");
-
-            // Assuming you have a way to select an account for the transfer.
-            Console.Write("Enter From Account Number: ");
-            if (int.TryParse(Console.ReadLine(), out int fromAccountNumber))
-            {
-                var fromAccount = _client.Accounts.FirstOrDefault(a => a.Number == fromAccountNumber);
-
-                if (fromAccount != null)
-                {
-                    Console.Write("Enter Amount: ");
-                    if (decimal.TryParse(Console.ReadLine(), out decimal amount))
-                    {
-                        Console.Write("Enter To Account Number: ");
-                        if (int.TryParse(Console.ReadLine(), out int toAccountNumber))
-                        {
-                            bool result = _clientService.Transfer(fromAccount, amount, toAccountNumber);
-                            Console.WriteLine(result ? "Transfer successful." : "Transfer failed.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid to account number.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid amount.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("From account not found.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid from account number.");
-            }
-
-            Console.WriteLine("Press Enter to continue...");
-            Console.ReadLine();
+            Console.WriteLine(message);
         }
     }
+
 }
