78787b5b591ca91540f704abf2fad4da84a7573d
Yarub1 <144337494+Yarub1@users.noreply.github.com>
2023-11-24 10:54:19 +0100

    Update AdminPresenter.cs

--- UntitledBankApp/Presenters/AdminPresenter.cs
+++ UntitledBankApp/Presenters/AdminPresenter.cs
@@ -1,4 +1,5 @@
-namespace UntitledBankApp.Presenters;
+using UntitledBankApp;
+using UntitledBankApp.Views;
 
 public class AdminPresenter : Presenter
 {
@@ -15,18 +16,58 @@ public class AdminPresenter : Presenter
 
     public override void HandlePresenter()
     {
-        new AdminView(new Admin("AdminFullName", "AdminUsername", "AdminPassword"))
-               .ShowAdminMenu();
+        _adminView.ShowMessage("Welcome to the Admin Panel!");
 
-        Console.WriteLine("Enter your choice: ");
-        if (int.TryParse(Console.ReadLine(), out int choice))
+        int adminChoice;
+        do
         {
-            new AdminView(new Admin("AdminFullName", "AdminUsername", "AdminPassword"))
-                .HandleAdminInput(choice, new AdminService(new PseudoDb()));
+            adminChoice = _adminView.GetAdminChoice();
+
+            // Perform the selected action
+            switch (adminChoice)
+            {
+                case 1:
+                    HandleCreateUser();
+                    break;
+                case 2:
+                    HandleSetCurrencyRate();
+                    break;
+                case 3:
+                    _adminView.ShowMessage("Going back...");
+                    break;
+                default:
+                    _adminView.ShowMessage("Invalid choice. Please select a valid option.");
+                    break;
+            }
+        } while (adminChoice != 3);
+    }
+
+    private void HandleCreateUser()
+    {
+        try
+        {
+            _adminView.GetUserInputForNewUser(out Role role, out string fullName, out string username, out string password);
+            bool createUserResult = _adminService.CreateUser(role, fullName, username, password);
+            _adminView.UpdateUserCreationResult(createUserResult);
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("Invalid input. Please enter a number.\n");
+            _adminView.ShowMessage($"Error: {ex.Message}");
+        }
+    }
+
+    private void HandleSetCurrencyRate()
+    {
+        try
+        {
+            _adminView.GetUserInputForSettingCurrencyRate(out CurrencyCode currencyCode, out float rate);
+            _adminService.SetCurrencyRate(currencyCode, rate);
+            _adminView.UpdateCurrencyRateResult(true);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            _adminView.ShowMessage($"Error: {ex.Message}");
+            _adminView.UpdateCurrencyRateResult(false);
         }
     }
 }
